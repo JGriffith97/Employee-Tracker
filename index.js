04b0ea9e6ff7ac4acb13ba4f38a8a93feb044721
Jayden Griffith <griffithjayden97@outlook.com>
2022-07-12 16:30:09 -0600

    Added sequelize module, and created connection.js in ./config/

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer'); // Version inquirer@8.2.4 to avoid issues
 const cTable = require('console.table');
-require('dotenv').config()
+const sequelize = require('./config/connection')
 
 // Need to npm install the above.
 // Connection required to utilize the mysql import
