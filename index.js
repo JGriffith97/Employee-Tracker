d480f87f7dd7ad4b065cda68ca52883a38d85398
Jayden Griffith <griffithjayden97@outlook.com>
2022-07-12 16:16:01 -0600

    Added dotenv module, added .env file and example file.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer'); // Version inquirer@8.2.4 to avoid issues
 const cTable = require('console.table');
+require('dotenv').config()
 
 // Need to npm install the above.
 // Connection required to utilize the mysql import
