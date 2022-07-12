3d66866fef0d451de4b3af711f7523f9a1b9a1ac
Jayden Griffith <griffithjayden97@outlook.com>
2022-07-12 16:49:00 -0600

    Added initialMenu for inquirer prompt

--- index.js
+++ index.js
@@ -4,5 +4,21 @@ const inquirer = require('inquirer'); // Version inquirer@8.2.4 to avoid issues
 const cTable = require('console.table');
 const sequelize = require('./config/connection')
 
-// Need to npm install the above.
 // Connection required to utilize the mysql import
+// App will be able to create and read tables via inquirer, console.table, mySQL and the command prompt.
+
+const initialMenu = [
+  {
+    type: 'list',
+    name: 'optionSelection',
+    message: 'What would you like to do?',
+    choices: ['View All Employees', 
+              'Add Employee',
+              'Update Employee Role',
+              'View All Roles',
+              'Add Role',
+              'View All Departments',
+              'Add Department',
+              'Quit']
+  }
+];
