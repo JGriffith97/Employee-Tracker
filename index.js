4b952cbfd856049ce01b15da5c8e5d0d6dc6d0eb
Jayden Griffith <griffithjayden97@outlook.com>
2022-07-12 16:06:00 -0600

    Added node_modules to the gitignore. initialized npm, and installed necessary modules.

---
+++ index.js
@@ -0,0 +1,7 @@
+// Required packages
+const mysql = require('mysql2');
+const inquirer = require('inquirer'); // Version inquirer@8.2.4 to avoid issues
+const cTable = require('console.table');
+
+// Need to npm install the above.
+// Connection required to utilize the mysql import
