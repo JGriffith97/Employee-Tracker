0101f3edba70c1400eaf43314d35b31b9e2423be
Jayden Griffith <griffithjayden97@outlook.com>
2022-07-12 17:14:05 -0600

    Added conditions framework for logic regarding the inquirer prompt.

--- index.js
+++ index.js
@@ -5,20 +5,47 @@ const cTable = require('console.table');
 const sequelize = require('./config/connection')
 
 // Connection required to utilize the mysql import
-// App will be able to create and read tables via inquirer, console.table, mySQL and the command prompt.
+// App will be able to create and read tables via inquirer, console.table, mySQL 
+// and the command prompt.
 
+// Depending on the choice, show, update or add to relevant tables. Repeat unless 
+// Quit.
 const initialMenu = [
   {
     type: 'list',
     name: 'optionSelection',
     message: 'What would you like to do?',
     choices: ['View All Employees', 
-              'Add Employee',
-              'Update Employee Role',
               'View All Roles',
-              'Add Role',
               'View All Departments',
+              'Update Employee Role',
+              'Add Employee',
+              'Add Role',
               'Add Department',
               'Quit']
   }
 ];
+
+function inqPrompt() {
+  inquirer
+  .prompt(initialMenu)
+    .then((answers) => {
+      if (answers.optionSelection === 'View All Employees') {
+
+      } else if (answers.optionSelection === 'View All Roles') {
+
+      } else if (answers.optionSelection === 'View All Departments') {
+
+      } else if (answers.optionSelection === 'Update Employee Role') {
+
+      } else if (answers.optionSelection === 'Add Employee') {
+
+      } else if (answers.optionSelection === 'Add Role') {
+
+      } else if (answers.optionSelection === 'Add Department') {
+
+      } else if (answers.optionSelection === 'Quit') {
+        return
+      }
+    })
+}
