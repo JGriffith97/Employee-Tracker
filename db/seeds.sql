552e6062a2354f4923c00aa2053a9a9c408fd842
Jayden Griffith <griffithjayden97@outlook.com>
2022-07-13 20:20:03 -0600

    Reconfigued the seeds.sql file.

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,18 +7,22 @@ VALUES ("Sales"),
 
 INSERT INTO role (title, salary, department_id)
 VALUES ("Sales Lead", 100000, 1),
-       ("Sales Associate", 60000, 2),
-       ("Lead Engineer", 150000, 3),
-       ("Software Engineer", 120000, 4);
-       ("Manager", 160000, 5);
-
-
-INSERT INTO employee (first_name, last_name, role_id)
-VALUES ("Greg", "Franks", 5);
+       ("Sales Associate", 60000, 1),
+       ("Lead Engineer", 150000, 2),
+       ("Software Engineer", 120000, 2),
+       ("Account Manager", 160000, 3),
+       ("Accountant", 110000, 3),
+       ("Legal Team Lead", 200000, 4),
+       ("Lawyer", 140000, 4);
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Wilkins", 1, 1),
-       ("Thomas", "James", 2, null),
-       ("Kevin", "Books", 3, null),
-       ("Sarah", "Graves", 4, null);
+VALUES ("John", "Wilkins", 1, null),
+       ("Thomas", "James", 1, 1),
+       ("Kevin", "Books", 2, null),
+       ("Sarah", "Graves", 2, 2),
+       ("Tony", "Wagner", 3, null),
+       ("Elliot", "Mendez", 3, 3),
+       ("Lyla", "Indra", 4, null),
+       ("Charles", "Thomas", 4, 4);
+       
