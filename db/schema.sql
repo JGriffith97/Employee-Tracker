a42edb1d430d43c90163ffb1b931d942a79d9380
Jayden Griffith <griffithjayden97@outlook.com>
2022-07-13 20:27:09 -0600

    Renamed the database.

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS employeeTracker_db;
-CREATE DATABASE employeeTracker_db;
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
 
-USE employeeTracker_db;
+USE employees_db;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
